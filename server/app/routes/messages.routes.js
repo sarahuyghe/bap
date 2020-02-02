1609987bb0795ee7479961b7c15b72ab8436ff3f
sarahuyghe <sara.huyghe@student.howest.be>
2020-02-02 22:26:31 +0100

    added api routes to routes

--- server/app/routes/messages.routes.js
+++ server/app/routes/messages.routes.js
@@ -1,8 +1,8 @@
 module.exports = app => {
 	const controller = require("../controllers/messages.controller.js");
-	app.post("/messages", controller.create);
-	app.get("/messages", controller.findAll);
-	app.get("/messages/:teamId", controller.findOne);
+	app.post("/api/messages", controller.create);
+	app.get("/api/messages", controller.findAll);
+	app.get("/api/messages/:teamId", controller.findOne);
 	// app.put("/messages/:teamId", controller.update);
 	// app.delete("/messages/:teamId", controller.delete);
 };
