ca0db9679100a2192fef9893da8eb3e4c9c9e4f5
sarahuyghe <sara.huyghe@student.howest.be>
2020-02-07 19:14:13 +0100

    added customisation bottle

--- client/src/components/team/Custom.jsx
+++ client/src/components/team/Custom.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import styles from "./TeamForm.module.css";
 
-// import bottleGrey from "./../../images/bottle.png";
-// import bottleRed from "./../../images/bottle2.png";
-// import bottleblue from "./../../images/bottle3.png";
-// import bottlePink from "./../../images/bottle4.png";
+import bottleGrey from "./../../images/bottle.png";
+import bottleRed from "./../../images/bottle2.png";
+import bottleblue from "./../../images/bottle3.png";
+import bottlePink from "./../../images/bottle4.png";
 
 import capPink from "./../../images/cap1.png";
 import capBrown from "./../../images/cap2.png";
@@ -19,40 +19,58 @@ class Custom extends Component {
 					id: 1,
 					name: "bottleGrey",
 					color: "grey",
-					img: " /../../images/bottle.png"
+					img: bottleGrey
 				},
 				{
 					id: 2,
 					name: "bottleRed",
 					color: "red",
-					img: " /../../images/bottle2.png"
+					img: bottleRed
 				},
 				{
 					id: 3,
 					name: "bottleblue",
 					color: "blue",
-					img: " /../../images/bottle3.png"
+					img: bottleblue
 				},
 				{
 					id: 4,
 					name: "bottlePink",
 					color: "pink",
-					img: " /../../images/bottle4.png"
+					img: bottlePink
 				}
 			],
-			selectedImageIndex: 0
+			imagesCap: [
+				{
+					id: 1,
+					name: "capPink",
+					color: "grey",
+					img: capPink
+				},
+				{
+					id: 2,
+					name: "capBrown",
+					color: "red",
+					img: capBrown
+				},
+				{
+					id: 3,
+					name: "cap",
+					color: "blue",
+					img: cap
+				}
+			],
+			currentImg: 0,
+			currentImgCap: 0
 		};
 	}
 
 	handleChange = e => {
-		console.log(e.target.index);
-		this.setState({ selectedImageIndex: e.target.dataset.index });
-		// bottle = e.currentTarget.value;
-		console.log(this.state.selectedImageIndex);
+		this.setState({ currentImg: e.currentTarget.value });
 	};
 
 	render() {
-		const { bottle, images } = this.state;
+		const { currentImg, images, imagesCap, currentImgCap } = this.state;
 		return (
 			<>
 				<h3>1. Boobottle personaliseren</h3>
@@ -64,22 +82,30 @@ class Custom extends Component {
 					</p>
 					<p>Omdat elk team uniek is</p>
 				</div>
-				<select onChange={this.handleChange}>
-					{images.map((img, index) => (
-						// console.log(img);
-						<option key={index} value={img.name}>
-							{img.color}
-						</option>
-					))}
-					{/* <option value="bottleGrey">Grey</option>
-					<option value="bottleRed">Red</option>
-					<option value="bottleblue">Blue</option>
-					<option value="bottlePink">Pink</option> */}
-				</select>
-				<div className={styles.bottleCustom}>
-					{/* {if()} */}
-					<img src={capPink} alt="" width="240" />
-					<img src={bottle} alt="" width="160" id="bottle" />
+				<div>
+					<select onChange={this.handleChange}>
+						{images.map((img, index) => (
+							<option key={img.id} value={index}>
+								{img.color}
+							</option>
+						))}
+					</select>
+					<select
+						onChange={e =>
+							this.setState({ currentImgCap: e.currentTarget.value })
+						}
+					>
+						{imagesCap.map((img, index) => (
+							<option key={img.id} value={index}>
+								{img.color}
+							</option>
+						))}
+					</select>
+					<div className={styles.bottleCustom}>
+						{/* {if()} */}
+						<img src={imagesCap[currentImgCap].img} alt="" width="240" />
+						<img src={images[currentImg].img} alt="" width="160" id="bottle" />
+					</div>
 				</div>
 				<a href="#sec2" className={styles.submit}>
 					Volgende
