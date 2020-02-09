53af3c17ba6f77cf668c9e02e35a21c83dfa4860
sarahuyghe <sara.huyghe@student.howest.be>
2020-02-09 10:14:58 +0100

    online test

--- server/index.js
+++ server/index.js
@@ -22,26 +22,26 @@ mongoose
 
 const app = express();
 
-// app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-	res.json({ message: "up and first running" });
-});
-
-// app.get("*", (req, res) => {
-// 	res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-// });
-
 require("./app/routes/teams.routes.js")(app);
 require("./app/routes/participants.routes.js")(app);
 require("./app/routes/messages.routes.js")(app);
 require("./app/routes/auth.routes.js")(app);
 require("./app/routes/mails.routes.js")(app);
+
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
+
+app.get("/", (req, res) => {
+	res.json({ message: "up and first running" });
+});
 
 app.listen(process.env.PORT, () => {
 	console.log(`Server luistert op poort ${process.env.PORT}`);
