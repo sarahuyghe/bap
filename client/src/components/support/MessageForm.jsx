999a6e5ea550b8ee0f3de43cdec6803783de6bf4
sarahuyghe <sara.huyghe@student.howest.be>
2020-02-09 02:09:56 +0100

    Added stylin to message form

--- client/src/components/support/MessageForm.jsx
+++ client/src/components/support/MessageForm.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { inject, observer } from "mobx-react";
 
+import styles from "./MessageForm.module.css";
+
 const MessageForm = ({ teamStore, messageStore }) => {
 	const { teams } = teamStore;
 	const messageInput = React.createRef();
@@ -31,17 +33,19 @@ const MessageForm = ({ teamStore, messageStore }) => {
 	return (
 		<>
 			<form onSubmit={handleSubmit}>
-				<section className="{styles.panel}">
+				<section className={styles.panel}>
 					<h3>1. Selecteer een team</h3>
 					<div className="deeltitel">Message in a BOOBottle</div>
 					<p>Dit is het team dat uw steun bericht zal ontvangen</p>
-					<select ref={teamInput}>
-						{teams.map(team => (
-							<option key={team.id} value={team.id}>
-								{team.teamnaam}
-							</option>
-						))}
-					</select>
+					<section className={styles.formSection}>
+						<select ref={teamInput}>
+							{teams.map(team => (
+								<option key={team.id} value={team.id}>
+									{team.teamnaam}
+								</option>
+							))}
+						</select>
+					</section>
 					<a href="#sec2" className="secondaireButton">
 						Volgende
 					</a>
@@ -49,26 +53,37 @@ const MessageForm = ({ teamStore, messageStore }) => {
 				<section id="sec2">
 					<h3>2. Inhoud van het steunbericht</h3>
 					<div className="deeltitel">Message in a BOOBottle</div>
-					<h3>Wat is uw naam?</h3>
-					<p>
-						Dit mag ook een koosnaam of roepnaam zijn. Dit is enkel zodat het
-						team weet van wie het steun bericht komt
-					</p>
-					<label>
-						Mijn naam is
-						<input type="text" name="Naam" ref={naamInput} />
-					</label>
-					<br />
-					<h3>Wat wilt u hen zeggen?</h3>
-					<p>Dit is het steunbericht dat uw geselecteerd team zal ontvangen</p>
-					<label>
-						<input
-							type="text"
-							name="teamnaam"
-							placeholder="hey ..."
-							ref={messageInput}
-						/>
-					</label>
+
+					<section className={styles.formSection}>
+						<div>
+							<h4>Wat is uw naam?</h4>
+							<p className="uitlegTitle">
+								Dit mag ook een koosnaam of roepnaam zijn. Dit is enkel zodat
+								het team weet van wie het steun bericht komt
+							</p>
+							<label>
+								Mijn naam is
+								<input
+									type="text"
+									name="Naam"
+									className="form_input"
+									ref={naamInput}
+								/>
+							</label>
+						</div>
+						<h3>Wat wilt u hen zeggen?</h3>
+						<p className="uitlegTitle">
+							Dit is het steunbericht dat uw geselecteerd team zal ontvangen
+						</p>
+						<label>
+							<input
+								type="text"
+								name="teamnaam"
+								placeholder="hey ..."
+								ref={messageInput}
+							/>
+						</label>
+					</section>
 				</section>
 				<section id="sec3">
 					<h3>3. Wilt u alsnog sponseren?</h3>
@@ -111,7 +126,11 @@ const MessageForm = ({ teamStore, messageStore }) => {
 							<label>€15</label>
 						</div>
 					</>
-					<input type="submit" value="bevestigen" />
+					<input
+						type="submit"
+						value="bevestigen"
+						className="secondaireButton"
+					/>
 				</section>
 			</form>
 		</>
